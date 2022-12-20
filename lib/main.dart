2b2aa78a483e5c64d23a228a63dda4e8c1f03a34
fayiehub <72041788+fayiehub@users.noreply.github.com>
2022-12-20 12:03:10 +0300

    fourth commit

--- lib/main.dart
+++ lib/main.dart
@@ -44,65 +44,150 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
       drawer: Icon(Icons.menu),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(45.0),
-            child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
+      body: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Points",
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold)),
+                    Text(
+                      "1248",
+                      style:
+                          TextStyle(fontSize: 21, fontWeight: FontWeight.w700),
+                    )
+                  ]),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text("Points",
+                  Text("Earnings",
                       style:
                           TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
-                  Text(
-                    "1248",
-                    style: TextStyle(fontSize: 27, fontWeight: FontWeight.w700),
-                  )
-                ]),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("Earnings",
-                    style:
-                        TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
-                Text("History",
-                    style:
-                        TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
-                Text("Redeem",
-                    style:
-                        TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
-                Text("Info",
-                    style:
-                        TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
-              ],
+                  Text("History",
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+                  Text("Redeem",
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+                  Text("Info",
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+                ],
+              ),
             ),
-          ),
-          Column(
-            children: [
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(38.0),
-                  child: Text("How It Works",
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.w700)),
-                ),
+            Container(
+              width: 250,
+              child: Column(
+                children: [
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(38.0),
+                      child: Text("How It Works",
+                          style: TextStyle(
+                              fontSize: 21, fontWeight: FontWeight.w700)),
+                    ),
+                  ),
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(18.0),
+                      child: RichText(
+                          text: TextSpan(
+                              text: "One Point ",
+                              style: TextStyle(
+                                  fontSize: 14, fontWeight: FontWeight.w700),
+                              children: <TextSpan>[
+                            TextSpan(
+                                text: "is equivalent to ",
+                                style: TextStyle(
+                                    fontSize: 14, fontWeight: FontWeight.w500)),
+                            TextSpan(
+                              text: "Ksh 125 \n",
+                              style: TextStyle(
+                                  fontSize: 14, fontWeight: FontWeight.w700),
+                            ),
+                            TextSpan(
+                              text: "1 point earned = Ksh 1",
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            )
+                          ])),
+                    ),
+                  ),
+                  Container(
+                    width: 200,
+                    child: Divider(
+                      thickness: 1,
+                      color: Color.fromARGB(138, 48, 47, 47),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: Center(
+                        child: RichText(
+                      text: TextSpan(
+                          text:
+                              "Spend over Ksh 1000 per order and earn double the points",
+                          style: TextStyle(
+                              fontSize: 14, fontWeight: FontWeight.w500)),
+                    )),
+                  ),
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(18.0),
+                      child: RichText(
+                          text: TextSpan(
+                              text: "You have referred ",
+                              style: TextStyle(
+                                  fontSize: 14, fontWeight: FontWeight.w500),
+                              children: <TextSpan>[
+                            TextSpan(
+                                text: "10 ",
+                                style: TextStyle(
+                                    fontSize: 14, fontWeight: FontWeight.w700)),
+                            TextSpan(
+                              text: "friends",
+                              style: TextStyle(
+                                  fontSize: 14, fontWeight: FontWeight.w500),
+                            ),
+                          ])),
+                    ),
+                  ),
+                  Container(
+                    height: 40,
+                    width: 250,
+                    child: FloatingActionButton(
+                      onPressed: () {
+                        // code to execute when the button is pressed
+                      },
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      backgroundColor: Color.fromARGB(255, 255, 127, 7),
+                      child: Text('Share your link',
+                          style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black)),
+                    ),
+                  )
+                ],
               ),
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(18.0),
-                  child: Text(
-                      "One Point is equivalent to Ksh 125 1 Point earned = Ksh 1",
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.w700)),
-                ),
-              ),
-            ],
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
